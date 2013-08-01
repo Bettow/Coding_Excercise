87d48ec6ddb41fb98e37455115bd93680bda7b41
Bettow <bettow79@gmail.com>
2013-07-31 20:44:40 -0700

    Update DivideTwoIntegers.cpp

--- Coding_Exercise/LeetCode/DivideTwoIntegers.cpp
+++ Coding_Exercise/LeetCode/DivideTwoIntegers.cpp
@@ -1,7 +1,7 @@
 int divide(int dividend, int divisor) {
 	// Start typing your C/C++ solution below
 	// DO NOT write int main() function
-	int long long d = dividend;
+	int long long d = dividend;  //Use long long int
 	int long long c = divisor;
 
 	bool isNeg = false;
@@ -16,7 +16,7 @@ int divide(int dividend, int divisor) {
 	}
 
 	long long int ret = 0;
-	while (d >= c) {
+	while (d >= c) {  //Don't use d > 0, use this for case 1/2
 		long long int cc = c;
 		int i =0;
 		while (d >= cc) {
