c4d4ec29dd9d303412c9d83d77fddf6f8fabba7f
Bettow <bettow79@gmail.com>
2013-08-04 14:55:53 -0700

    Update UniquePathsII.cpp

--- Coding_Exercise/LeetCode/UniquePathsII.cpp
+++ Coding_Exercise/LeetCode/UniquePathsII.cpp
@@ -1,43 +1,29 @@
-	int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
-		// Start typing your C/C++ solution below
-		// DO NOT write int main() function
-
-		int m = obstacleGrid.size();
-		int n = obstacleGrid[0].size();
-		if (!m || !n)
-			return 0;        
-
-		vector<vector<int> > tb(m, vector<int>(n,0));
-		if (obstacleGrid[0][0]==1)
-			return 0;
-		else
-			tb[0][0] = 1;  //should set this
-
-		for (int i =1; i <n ; ++i) {
-			if (obstacleGrid[0][i] ==1 ||
-				tb[0][i-1] == 0)
-				tb[0][i]=0;
-			else
-				tb[0][i]=1;
-
-		}
-		for (int i =1; i <m ; ++i) {
-			if (obstacleGrid[i][0] ==1 ||
-				tb[i-1][0] == 0)
-				tb[i][0]=0;
-			else
-				tb[i][0]=1;                
-		}
-
-		for (int i = 1; i <m; ++i) {
-			for (int j =1 ; j < n; ++j) {
-				if (obstacleGrid[i][j]==1)
-					tb[i][j]=0;
-				else
-					tb[i][j] = tb[i-1][j]+ tb[i][j-1];      
-
-			}
-		}
-		return tb[m-1][n-1];
-
-	}
+    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int> > dp(m, vector<int>(n, 0));
+        for (int i =0; i < m; ++i) {
+            if (obstacleGrid[i][0] == 0)
+                dp[i][0] = 1;
+            else 
+                break;
+        }
+        for (int i =0; i < n; ++i) {
+            if (obstacleGrid[0][i] == 0)
+                dp[0][i] = 1;
+            else 
+                break;
+        }
+        for (int i =1; i < m; ++i) {
+            for (int j = 1; j< n; ++j) {
+                if (obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0;
+                else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
