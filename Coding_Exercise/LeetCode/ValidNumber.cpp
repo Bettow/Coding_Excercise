4bf9f3489530fa08c59ab347c7b376f7e0e82b5d
Bettow <bettow79@gmail.com>
2013-08-04 14:12:04 -0700

    Update ValidNumber.cpp

--- Coding_Exercise/LeetCode/ValidNumber.cpp
+++ Coding_Exercise/LeetCode/ValidNumber.cpp
@@ -1,22 +1,37 @@
-bool isMatch(const char *s, const char *p) {
-	// Start typing your C/C++ solution below
-	// DO NOT write int main() function    
-	if (!*p)
-		return *s== 0;
-
-	if (*(p+1) == '*') {
-		while (*s == *p || *p== '.' && *s != 0) {
-			if (isMatch(s+2, p))
-				return true;
-			s++;
-		}
-		return isMatch(s, p+2);
-
-	} else {
-		if (*p == *s || *p == '.' && *s != 0)
-			return isMatch(s+1, p+1);
-		else 
-			return false;
-	}
-
-}
+    bool isNumber(const char *s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        bool is_num= false;
+        bool is_dot =false;
+        bool is_e =  false;
+        bool is_space = false;
+        while (*s && *s == ' ') s++;
+        if (*s && (*s == '+' || *s == '-')) s++;
+        while(*s) {
+            if (*s == ' ')
+                is_space = true;
+            else if (is_space) {
+                return false;
+            } else if (isdigit(*s)) {
+                is_num = true;
+            } else if (*s == '.') {
+                if (is_dot || is_e)
+                    return false;
+                is_dot = true;
+            } else if (*s == 'e') {
+                if (is_num && !is_e) { //&& here
+                    is_e = true;
+                    is_num = false;  //for case: 1e
+                } else 
+                    return false;
+            } else if (*s == '+' || *s == '-') {
+                if (*(s-1) != 'e')
+                    return false;
+            } else {
+                return false;
+            }
+            s++;
+        }
+        return is_num;
+        
+    }
