a1ad178976a5a3971d61bc49e611d6405e834257
Bettow <bettow79@gmail.com>
2013-08-04 10:38:40 -0700

    Update MinimumDepthOfBinaryTree.cpp

--- Coding_Exercise/LeetCode/MinimumDepthOfBinaryTree.cpp
+++ Coding_Exercise/LeetCode/MinimumDepthOfBinaryTree.cpp
@@ -1,17 +1,23 @@
+    int getDepth(TreeNode *root, int lv) {
+        if (!root) 
+            return lv-1;
+        
+        int l = getDepth(root->left, lv+1);
+        int r = getDepth(root->right, lv+1);
+        
+        if (l == lv && r == lv)
+            return lv;
+        else if (l == lv)
+            return r;
+        else if (r == lv)
+            return l;
+        else 
+            return min(r, l);
+        
+        
+    }
     int minDepth(TreeNode *root) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        return getDepth(root, 0);
-    }
-    int getDepth(TreeNode *node, int depth) {
-        if (node == NULL)
-            return depth;
-        if (!node->left && !node->right)
-            return depth+1;
-        
-        
-        int leftDepth = node->left? getDepth(node->left, depth+1):INT_MAX;
-        int rightDepth = node->right? getDepth(node->right, depth+1):INT_MAX;
-        return min(leftDepth, rightDepth);
-        
+        return getDepth(root, 1);
     }
