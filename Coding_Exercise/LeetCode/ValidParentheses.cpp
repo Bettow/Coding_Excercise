cd049301c3a723cd1a708fb5b0012fb011cbf4da
Bettow <bettow79@gmail.com>
2013-08-17 11:30:27 -0700

    Update ValidParentheses.cpp

--- Coding_Exercise/LeetCode/ValidParentheses.cpp
+++ Coding_Exercise/LeetCode/ValidParentheses.cpp
@@ -1,37 +1,22 @@
-	bool isValid(string s) {
-		// Start typing your C/C++ solution below
-		// DO NOT write int main() function
-		stack<char> char_s;
-		for (int i = 0; i < s.size(); ++i) {
-			if (s[i] == '(' ||
-				s[i] == '[' ||
-				s[i] == '{' )
-				char_s.push(s[i]);
-		else {
-			if (s[i] == ')') {
-				if (!char_s.empty() && char_s.top() == '(') {  //BUG1 should use top instead of pop.
-					char_s.pop();
-				} else {
-					return false;
-				}
-			}
-			if (s[i] == ']') {
-				if (!char_s.empty() && char_s.top() == '[') {
-					char_s.pop();
-				} else {
-					return false;
-				}
-			}
-			if (s[i] == '}') {
-				if (!char_s.empty() && char_s.top() == '{') {
-					char_s.pop();
-				} else {
-					return false;
-				}
-			}
-		}
-
-	}
-	return char_s.empty() ? true: false;
-
-	}
+    bool isValid(string s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        unordered_map<char, char> bmap;
+        stack<char> stack_c;
+        bmap['('] = ')';
+        bmap['['] = ']';
+        bmap['{'] = '}';
+        for (int i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            if (bmap.count(c)) {
+                stack_c.push(c);
+            } else {
+                if (stack_c.empty() || bmap[stack_c.top()] != c){
+                    return false;
+                } else {
+                    stack_c.pop();
+                }
+            }
+        }
+        return stack_c.empty()? true: false;
+    }
