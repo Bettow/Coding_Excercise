9d92c90615bd641072c9255664a4cf05a387eed7
Bettow <bettow79@gmail.com>
2013-08-26 22:41:56 -0700

    Update MergekSortedLists.cpp

--- Coding_Exercise/LeetCode/MergekSortedLists.cpp
+++ Coding_Exercise/LeetCode/MergekSortedLists.cpp
@@ -1,38 +1,30 @@
-	ListNode *mergeKLists(vector<ListNode *> &lists) {
-		// Start typing your C/C++ solution below
-		// DO NOT write int main() function
-		if (lists.empty()) return NULL;
-
-		vector<ListNode*> hp;
-		for (int i =0; i < lists.size(); ++i) {
-			if (lists[i]) {  //test NULL of list
-				hp.push_back(lists[i]);
-			}
-		}
-
-		if (hp.empty()) return NULL;
-
-		make_heap(hp.begin(), hp.end(), comp());
-		ListNode *dummy = new ListNode(0);
-		ListNode *node = dummy;
-		while(!hp.empty()) {
-			ListNode *v = hp.front(); 
-			pop_heap(hp.begin(), hp.end(), comp()); 
-			hp.pop_back();
-			node->next = new ListNode(v->val);
-			v= v->next;
-			node =  node->next;
-			if (v) {
-				hp.push_back(v);
-				push_heap(hp.begin(), hp.end(), comp());
-			}
-		}
-		return dummy->next;
-	}
-
-	class comp {
-	public:
-		bool operator()(ListNode *l1, ListNode* l2) const {
-			return l1->val > l2->val;  //Why >
-		}
-	};
+    class comp {
+        public:
+        bool operator()(ListNode *l, ListNode *r) {
+            return l->val > r->val;   //Return larger for head and priority queue
+        }
+    };
+    
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (lists.empty())
+            return NULL;
+        
+        priority_queue<ListNode*, vector<ListNode*>, comp> pq;
+        for (int i = 0; i < lists.size(); ++i) {
+            if (lists[i])
+                pq.push(lists[i]);
+        }
+        ListNode *dummy_head = new ListNode(0);
+        ListNode *curr = dummy_head;
+        while(!pq.empty()){
+            ListNode *n = pq.top();
+            pq.pop();
+            curr->next = n;
+            if (n->next)
+                pq.push(n->next);
+            curr = curr->next;
+        }
+        return dummy_head->next;
+    }
