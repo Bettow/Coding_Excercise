a68dea500cc7640ba5eb16ab997481e4e69526e7
Bettow <bettow79@gmail.com>
2013-09-09 20:16:12 -0700

    Update SubstringWithConcatenationOfAllWords.cpp

--- Coding_Exercise/LeetCode/SubstringWithConcatenationOfAllWords.cpp
+++ Coding_Exercise/LeetCode/SubstringWithConcatenationOfAllWords.cpp
@@ -1,32 +1,31 @@
-	vector<int> findSubstring(string S, vector<string> &L) {
-		// Start typing your C/C++ solution below
-		// DO NOT write int main() function
-		vector<int> result;
-		map<string, int> words;
-		map<string, int> curs;
-
-		for (int i =0; i < L.size(); ++i) {
-			words[L.at(i)]++;
-		}
-
-		int M = L.size(); //m words
-		if (M == 0) return result;
-		int N = L.at(0).size(); //n size word
-		for (int i = 0; i <= S.size() -M*N; ++i) {
-			curs.clear();
-			int j = 0;
-			for (j = 0; j < M; ++j) { 
-				string word = S.substr(i + j*N, N);
-				if (words.find(word) == words.end()) {
-					break;
-				} else {
-					curs[word]++;
-				}
-				if (curs[word]> words[word])
-					break;
-			}
-			if (j == M)
-				result.push_back(i);
-		}
-		return result;
+vector<int> findSubstring(string S, vector<string> &L) {
+    // Start typing your C/C++ solution below
+	// DO NOT write int main() function
+	vector<int> ret;
+	if (S.empty() || L.empty())
+		return ret;
+	unordered_map<string, int> target;
+	for (int i =0; i < L.size(); ++i) {
+		target[L[i]]++;
 	}
+
+    	int n = S.size();  //using s.size() in the loop would cause a bug as S.size() is size_t
+	int nl = L.size();
+	int ll = L[0].size();
+	for (int i =0; i<= n - ll*nl ; i++) {
+		unordered_map<string, int> c(target);
+		string curr_s = S.substr(i, ll);
+		if (target.count(curr_s)){
+			int j=0;
+			for (; j < nl; j++) {
+				string cs = S.substr(i+j*ll, ll);
+				c[cs]--;
+				if (c[cs] <0) break;
+			}
+			if (j == nl) {
+				ret.push_back(i);
+			}
+		}
+	}
+	return ret;
+}
