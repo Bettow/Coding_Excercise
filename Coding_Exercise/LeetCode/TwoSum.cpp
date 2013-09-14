969d411cff768ac65a5a77a49895b3c089312054
Bettow <bettow79@gmail.com>
2013-09-14 10:12:17 -0700

    Update TwoSum.cpp

--- Coding_Exercise/LeetCode/TwoSum.cpp
+++ Coding_Exercise/LeetCode/TwoSum.cpp
@@ -1,33 +1,17 @@
-	vector<int> twoSum(vector<int> &numbers, int target) {
-		// Start typing your C/C++ solution below
-		// DO NOT write int main() function
-		vector<int> result(2, 0);
-
-		if(numbers.size() < 2){
-			return result;
-		}
-
-		map<int, int> imap;
-		for (int i = 0; i < numbers.size(); ++i) {
-			if (imap.find(numbers[i]) != imap.end())
-				imap[numbers[i]] = i;
-			else	
-				imap.insert(make_pair(numbers[i],i));  
-		}
-
-		for (int i = 0; i < numbers.size(); ++i) {
-			int indx2 = target - numbers[i];
-			if (imap.find(indx2) != imap.end()){
-				if (i <= imap[indx2]) {
-					result[0] = i+1;
-					result[1] = imap[indx2]+1;
-				} else {
-					result[0] = imap[indx2]+1;
-					result[1] = i+1;
-				}
-                return result;
-			}
-		}
-		//return if not found      
-
-	}
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        unordered_map<int, int> im;
+        for (int i =0; i< numbers.size(); ++i) {
+            im[numbers[i]] = i;
+        }
+        vector<int> r(2);
+        for (int i = 0; i < numbers.size(); ++i) {
+            int s = target - numbers[i];
+            if (im.count(s)) {
+                r[0] = i+1;
+                r[1] = im[s]+1;
+                return r;
+            }
+        }
+    }
