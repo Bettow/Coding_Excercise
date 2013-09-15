5c1809d6271829ea2d9250d69c4a0a1c41ddda64
Bettow <bettow79@gmail.com>
2013-09-15 08:33:34 -0700

    Update 4Sum.cpp

--- Coding_Exercise/LeetCode/4Sum.cpp
+++ Coding_Exercise/LeetCode/4Sum.cpp
@@ -1,31 +1,38 @@
-	vector<vector<int> > fourSum(vector<int> &num, int target) {
-		vector<vector<int> > ret;
-		if(num.size() < 4) return ret;
-		sort(num.begin(), num.end());
-		for(int i = 0; i < num.size(); ++i)
-			for(int j = i + 1; j < num.size(); ++j) {
-				int left = j + 1;
-				int right = (int)num.size() - 1;
-				int sum = 0;
-				while(left < right){
-					sum=num[i]+num[j]+num[left]+num[right];
-					if(target == sum){
-						ret.push_back(vector<int>(4));
-						ret.back()[0]=num[i];
-						ret.back()[1]=num[j];
-						ret.back()[2]=num[left];
-						ret.back()[3]=num[right];
-						++left;
-						--right;
-					}
-					else if(sum > target)
-						--right;
-					else
-						++left;
-				}
-			}
+    vector<vector<int> > fourSum(vector<int> &num, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int n = num.size();
+        vector<vector<int> > ret;
 
-		sort(ret.begin(), ret.end());
-		ret.resize(unique(ret.begin(),ret.end())-ret.begin());
-		return ret;
-	}
+        sort(num.begin(), num.end());
+        for (int i = 0; i < n; ++i) {
+            if (i != 0 && num[i] == num[i - 1]) continue;
+
+            for (int j = n - 1; j > i; --j) {
+                if (j != n - 1 && num[j] == num[j + 1]) continue;
+
+                int L = i + 1, R = j - 1;
+                while (L < R) {
+                    int sum = num[i] + num[L] + num[R] + num[j];
+                    if (L != i + 1 && num[L] == num[L - 1]) {
+                        L++;
+                    } else if (R != j - 1 && num[R] == num[R + 1]) {
+                        R--;
+                    } else if (sum > target) {
+                        R--;
+                    } else if (sum < target) {
+                        L++;
+                    } else {
+                        ret.resize(ret.size() + 1);
+                        ret.back().push_back(num[i]);
+                        ret.back().push_back(num[L]);
+                        ret.back().push_back(num[R]);
+                        ret.back().push_back(num[j]);
+                        L++;
+                        R--;
+                    }
+                }
+            }
+        }
+        return ret;
+    }
