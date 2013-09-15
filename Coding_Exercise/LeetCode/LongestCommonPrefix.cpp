fb39a23a1a2737feba2554ea9940e5d205aa6619
Bettow <bettow79@gmail.com>
2013-09-15 08:11:24 -0700

    Update LongestCommonPrefix.cpp

--- Coding_Exercise/LeetCode/LongestCommonPrefix.cpp
+++ Coding_Exercise/LeetCode/LongestCommonPrefix.cpp
@@ -1,29 +1,17 @@
-	string longestCommonPrefix(vector<string> &strs) {
-		// Start typing your C/C++ solution below
-		// DO NOT write int main() function
-		if (strs.empty())
-			return ("");
-		if (strs.size() ==1) {
-			return strs[0];
-		}
-
-		int i =0;
-		bool isFound = false;
-		for (i =0; i < strs[0].size(); ++i) {
-			for (int j =1; j < strs.size(); ++j) {
-				if (i >= strs[j].size() || strs[j][i] != strs[0][i]) {
-					isFound = true;
-					break;
-				}
-			}
-			if (isFound == true)
-				break;
-		}
-
-		//if (isFound) i--;  //BUG1
-		if (i ==0)
-			return ("");
-		else {
-			return strs[0].substr(0, i);
-		}
-	}
+    string longestCommonPrefix(vector<string> &strs) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (strs.empty()) return "";
+        int n = strs.size();
+        if (n == 1) return strs[0];
+        string s = strs[0];
+        for (int i =0; i < s.size(); ++i) {
+            char c = s[i];
+            for (int j = 1; j < n; ++j) {
+                if (strs[j].size() <= i || strs[j][i] != c) {
+                    return s.substr(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
