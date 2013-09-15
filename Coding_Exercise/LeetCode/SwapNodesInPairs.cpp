faddedc69ca51604a38b9e9b55d4a1dbffbb68c0
Bettow <bettow79@gmail.com>
2013-09-15 09:33:26 -0700

    Update SwapNodesInPairs.cpp

--- Coding_Exercise/LeetCode/SwapNodesInPairs.cpp
+++ Coding_Exercise/LeetCode/SwapNodesInPairs.cpp
@@ -7,3 +7,24 @@
 		head->next->next = swapPairs(head->next->next);
 		return head;
 	}
+	
+	//iterative
+	ListNode *swapPairs(ListNode *head) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (!head)
+            return NULL;
+        if (!head->next)
+            return head;
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *curr = dummy;
+        while (curr->next && curr->next->next) {
+            ListNode *t = curr->next;
+            curr->next = t->next;
+            t->next = t->next->next;
+            curr->next->next = t;
+            curr = curr->next->next;
+        }
+        return dummy->next;
+    }
