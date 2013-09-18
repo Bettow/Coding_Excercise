a13fbc1a9ef77c01d53819a58d53b120af63fad6
Bettow <bettow79@gmail.com>
2013-09-18 07:23:56 -0700

    Update LongestConsecutiveSequence.cpp

--- Coding_Exercise/LeetCode/LongestConsecutiveSequence.cpp
+++ Coding_Exercise/LeetCode/LongestConsecutiveSequence.cpp
@@ -1,27 +1,34 @@
-    int longestConsecutive(vector<int> &num) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        unordered_set<int> si;
-        for (int i = 0; i < num.size(); ++i) {
-            si.insert(num[i]);    
-        }
-        int ret = 0;
-        for (unordered_set<int>::iterator iter = si.begin(); iter != si.end(); ++iter) {
-            int l = getLength(si, *iter, false) + getLength(si, *iter+1, true);
-            ret = max(ret,l);
-        }
-        return ret;
-    }
-    
-    int getLength(unordered_set<int> si, int e, bool dir) {
-        int count = 0;
-        while (si.find(e) != si.end()) {
-            count++;
-            si.erase(si.find(e));  //key 
-            if (dir) 
-                e++;
-            else
-                e--;
-        }
-        return count;
-    }
+int get_len(unordered_set<int> &us, int d, int dir) {
+	int r = 0;
+	int count = 0;
+	while (us.count(d)) {
+		count++;
+		r = max(count, r);
+		us.erase(us.find(d));
+		if (dir) {
+			d++;
+		} else {
+			d--;
+		}
+
+	}
+	return r;
+}
+int longestConsecutive(vector<int> &num) {
+	// Start typing your C/C++ solution below
+	// DO NOT write int main() function
+	if (num.empty())
+		return 0;
+	unordered_set<int> us;
+	for (int i = 0; i < num.size(); ++i) {
+		us.insert(num[i]);
+	}
+	int r = 0;
+	while(!us.empty()) {
+		auto i = us.begin();
+		int d = get_len(us, *i+1, true) + get_len(us, *i, false);
+		r = max(r, d);
+	}
+	return r;
+
+}
