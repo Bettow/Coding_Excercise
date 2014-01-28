ad5b21a4c2a6896befa69109e428bc5420b0a795
Bettow <bettow79@gmail.com>
2014-01-28 08:46:40 -0800

    Update RemoveDuplicatesfromSortedArray.cpp

--- Coding_Exercise/LeetCode/RemoveDuplicatesfromSortedArray.cpp
+++ Coding_Exercise/LeetCode/RemoveDuplicatesfromSortedArray.cpp
@@ -1,14 +1,12 @@
-	int removeDuplicates(int A[], int n) {
-		if (n==0)
-			return 0;
-
-		int slow =0;
-		int fast =1;
-		while (fast <n) {
-			if (A[fast] != A[slow]) {
-				A[++slow] = A[fast];			
-			}
-			fast++;
-		}
-		return slow+1;
-	}
+    int removeDuplicates(int A[], int n) {
+        if (n ==0)
+            return 0;
+        int slow = 0;
+        for (int i = 1; i < n; ++i) {
+            if (A[i] != A[slow]) {
+                A[++slow] = A[i];
+            }
+            
+        }
+        return slow+1;
+    }
