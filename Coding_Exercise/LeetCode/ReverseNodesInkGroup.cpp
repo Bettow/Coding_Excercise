0dc43495ea4afa4928ae771454d9fc0c9a48ebc0
Bettow <bettow79@gmail.com>
2014-01-28 14:51:51 -0800

    Update ReverseNodesInkGroup.cpp

--- Coding_Exercise/LeetCode/ReverseNodesInkGroup.cpp
+++ Coding_Exercise/LeetCode/ReverseNodesInkGroup.cpp
@@ -1,36 +1,40 @@
 ListNode *reverseKGroup(ListNode *head, int k) {
-	// Start typing your C/C++ solution below
-	// DO NOT write int main() function
-	if (!head) return NULL;
-	int c = k;
-	ListNode *dummy = new ListNode(0);
-	dummy->next = head;
-	ListNode *pre = dummy;
-	ListNode *t = pre;
-	while (t && c) {
-		t= t->next;
-		c--;
-		if (!t)
-			break;
-		if (c==0) {
-			pre = revsersK(pre, t->next);  //pre will be changed
-			t = pre;
-			c = k;
-		}
+        if (!head)
+            return NULL;
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *tail = head;
+        int count = k;
+        while (count && tail) {
+            tail = tail->next;
+            count--;
+        }
+        if (!tail && count)  //Here
+            return head;
+        ListNode *ph = dummy;
+        count = k;
+        while(1) {
+            reverse(ph, tail);
+            int count = k;
+            while (tail && count) {
+                tail = tail->next;
+                ph = ph->next;
+                count--;
+            }
+            if (count && !tail)  //Exit condition,  Note that tail == NULL alone is NOT an exit condition
+                break;
+        } 
+        return dummy->next;
+    }
+    
+void reverse(ListNode* head, ListNode* tail) {
+	ListNode *p = head->next;
+	ListNode *pn = tail;
+	while (p != tail) {
+		head->next = p->next;
+		p->next = pn;
+		pn = p;
+		p = head->next;
 	}
-	return dummy->next;
-
-}
-ListNode* revsersK(ListNode *pre, ListNode *tail) {
-	ListNode *curr = pre->next;
-	ListNode *h = tail;
-	ListNode *ret = curr;
-	while (curr != tail) {
-		ListNode *n = curr->next;
-		curr->next = h;
-		h = curr;
-		curr = n;
-	}
-	pre->next = h;
-	return ret;
+	head->next = pn;
 }
