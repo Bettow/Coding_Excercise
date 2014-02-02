8ac1f84edc2ee2383153ec80dcc30f211e029f34
Bettow <bettow79@gmail.com>
2014-02-02 14:11:30 -0800

    Update MinimumPathSum.cpp

--- Coding_Exercise/LeetCode/MinimumPathSum.cpp
+++ Coding_Exercise/LeetCode/MinimumPathSum.cpp
@@ -23,3 +23,23 @@
 		}
 		return table[rowSize-1][colSize-1];        
 	}
+	
+	
+
+    int minPathSum(vector<vector<int> > &grid) {
+        int m = grid.size();
+        if (m == 0)
+            return 0;
+        int n = grid[0].size();
+        vector<vector<int> > table(m+1, vector<int>(n+1, INT_MAX));
+        table[0][0] = 0;
+        table[1][0] = 0;
+        table[0][1] = 0;
+        
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                table[i][j] = min(table[i-1][j], table[i][j-1]) + grid[i-1][j-1];
+            }
+        }
+        return table[m][n];
+    }
