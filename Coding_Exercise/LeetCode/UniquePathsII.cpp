9fe527c2c7ac7952b34848947f2042319445e502
Bettow <bettow79@gmail.com>
2014-02-02 14:28:25 -0800

    Update UniquePathsII.cpp

--- Coding_Exercise/LeetCode/UniquePathsII.cpp
+++ Coding_Exercise/LeetCode/UniquePathsII.cpp
@@ -27,3 +27,27 @@
         }
         return dp[m-1][n-1];
     }
+
+
+    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        int m = obstacleGrid.size();
+        if (m == 0)
+            return 0;
+        int n = obstacleGrid[0].size();
+        
+        vector<vector<int> > tb(m+1, vector<int>(n+1, 0));
+        
+        for (int i = 1; i <=m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (i == 1 && j == 1 && obstacleGrid[i-1][j-1] == 0) {
+                    tb[i][j] = 1;
+                } else if (obstacleGrid[i-1][j-1] == 1) {
+                    tb[i][j] = 0;
+                } else {
+                    tb[i][j] = tb[i-1][j] + tb[i][j-1];
+                }
+            }
+        }
+        
+        return tb[m][n];
+    }
