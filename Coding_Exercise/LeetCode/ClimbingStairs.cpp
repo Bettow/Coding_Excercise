72b8bfe0a28f7459186e05fbde473a3d6b00fa56
Bettow <bettow79@gmail.com>
2014-02-03 14:29:15 -0800

    Update ClimbingStairs.cpp

--- Coding_Exercise/LeetCode/ClimbingStairs.cpp
+++ Coding_Exercise/LeetCode/ClimbingStairs.cpp
@@ -1,15 +1,13 @@
 	int climbStairs(int n) {
-		// Start typing your C/C++ solution below
-		// DO NOT write int main() function
-		int p1= 2;  //BUG!!!
-		if (n ==1)  return 1;
-		int p2 =1 ;
-		if (n ==2) return 2;
-		int result =0;
-		for (int i = 3; i <=n; ++i) {
-			result = p1+p2;
-			p2 = p1;   //BUG!!!
-			p1 = result;
-		}
-		return result;
+        	int t1 = 0;
+        	int t2 = 1;
+        
+        	int ret = 0;
+        	while (n--) {
+            		ret = t1+t2;
+            		t1 = t2;
+            		t2  = ret;
+        	}
+        
+        	return ret;
 	}
