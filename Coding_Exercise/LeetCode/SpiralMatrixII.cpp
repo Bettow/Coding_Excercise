1be598d1c22da91754844a69b097202bb2c2ea53
Bettow <bettow79@gmail.com>
2014-02-02 20:52:26 -0800

    Update SpiralMatrixII.cpp

--- Coding_Exercise/LeetCode/SpiralMatrixII.cpp
+++ Coding_Exercise/LeetCode/SpiralMatrixII.cpp
@@ -1,22 +1,29 @@
-		vector<vector<int> > generateMatrix(int n) {
-			vector< vector<int> > matrix(n, vector<int>(n));
-			if (n == 0) return matrix;
-			int beginX = 0, endX = n - 1;
-			int beginY = 0, endY = n - 1;
-			int num = 1;
-			while (true) {
-				for (int i = beginX; i <= endX; ++i)
-					matrix[beginY][i] = num++;
-				if (++beginY > endY) break;
-				for (int i = beginY; i <= endY; ++i)
-					matrix[i][endX] = num++;
-				if (beginX > --endX) break;
-				for (int i = endX; i >= beginX; --i)
-					matrix[endY][i] = num++;
-				if (beginY > --endY) break;
-				for (int i = endY; i >= beginY; --i)
-					matrix[i][beginX] = num++;
-				if (++beginX > endX) break;
-			}
-			return matrix;
-		}
+    vector<vector<int> > generateMatrix(int n) {
+        vector<vector<int> > ret(n, vector<int>(n, 0));
+        
+        int l = 0;
+        int r = n-1;
+        
+        int num = 1;
+        while (l <=r) {
+            for (int i = l; i <= r; ++i) {
+                ret[l][i] = num++;
+            }
+            
+            for (int i = l+1; i <= r; ++i) {
+                ret[i][r] = num++;
+            }
+            
+            for (int i = r-1; i >= l; --i) {
+                ret[r][i] = num++;
+            }
+            
+            for (int i = r-1; i > l; --i) {
+                ret[i][l] = num++;
+            }
+            l++;
+            r--;
+        }
+        
+        return ret;
+    }
