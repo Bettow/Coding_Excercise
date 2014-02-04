144efabf530cae7b08aa7da54bb1b8e52f617728
Bettow <bettow79@gmail.com>
2014-02-04 11:14:58 -0800

    Update LargestRectangleInHistogram.cpp

--- Coding_Exercise/LeetCode/LargestRectangleInHistogram.cpp
+++ Coding_Exercise/LeetCode/LargestRectangleInHistogram.cpp
@@ -1,29 +1,24 @@
-	struct bar {
-		int height;
-		int loc;
-		bar(int h, int l): height(h), loc(l){}
-	};
-
     int largestRectangleArea(vector<int> &height) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (height.empty())
+        
+        if (height.empty()) 
+        {
             return 0;
-        stack<bar> sb;
-        int max_area = 0;
-        int n = height.size();
-        height.push_back(0);
-        for (int i =0; i <=n; ++i) {
-            if (!sb.empty() && sb.top().height > height[i]) {  //> or >== are the same
-                while (!sb.empty() && sb.top().height > height[i]) {
-                    bar b = sb.top();
-                    sb.pop();
-                    int l = sb.empty()? -1: sb.top().loc;
-                    int area = (i - l-1) * b.height; //i-l-1  //l is after pop need to -1
-                    max_area = max(max_area, area);
-                }                    
-            }
-            sb.push(bar(height[i], i));  //need to push in all cases
         }
-        return max_area;
+        stack<int> st;
+        
+        height.push_back(0);
+        
+        int ret = 0;
+        for (int i = 0; i < height.size(); ++i) {
+                    while (!st.empty() && height[i] < height[st.top()]) {
+                        
+                        int h = height[st.top()];
+                        st.pop();
+                        int l = st.empty()? -1:st.top();  //find previous top
+                        int area = (i-l -1) * h;  //use single bar to think
+                        ret = max(ret, area);
+                    }
+            st.push(i);
+        }
+        return ret;
     }
