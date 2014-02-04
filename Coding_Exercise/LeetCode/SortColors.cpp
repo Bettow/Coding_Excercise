06ab3e0c1b89863ebb6b57c29f2b98d1aebdac10
Bettow <bettow79@gmail.com>
2014-02-04 13:29:01 -0800

    Update SortColors.cpp

--- Coding_Exercise/LeetCode/SortColors.cpp
+++ Coding_Exercise/LeetCode/SortColors.cpp
@@ -1,19 +1,18 @@
-	void sortColors(int A[], int n) {
-		// Start typing your C/C++ solution below
-		// DO NOT write int main() function
-		int l = -1;
-		int r = n;
-		for (int i = 0; i < r;) {  //BUG
-			if (A[i] == 0) {
-				l++;
-				swap(A[i], A[l]);
-				i++;
-			} else if (A[i] == 2) {
-				r--;
-				swap(A[i], A[r]);
-			} else {
-				i++;
-			}
-
-		}
-	}
+    //l point to the first non 0 from left
+    //r point to the first non 2 from right
+    void sortColors(int A[], int n) {
+        int l = 0;
+        int r = n-1;
+        for (int i = 0; i <= r;) {
+            if (A[i] == 0){
+                swap(A[i], A[l]);
+                i++;
+                l++;
+            } else if (A[i] == 1) {
+                i++;
+            } else {
+                swap(A[i], A[r]);
+                r--;
+            }
+        }
+    }
