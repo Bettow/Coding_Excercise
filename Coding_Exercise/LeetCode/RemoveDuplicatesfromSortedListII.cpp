2a1a963d3a14f2ae14f57bf79f85157f1c4c5b59
Bettow <bettow79@gmail.com>
2014-02-04 12:59:43 -0800

    Update RemoveDuplicatesfromSortedListII.cpp

--- Coding_Exercise/LeetCode/RemoveDuplicatesfromSortedListII.cpp
+++ Coding_Exercise/LeetCode/RemoveDuplicatesfromSortedListII.cpp
@@ -1,22 +1,19 @@
     ListNode *deleteDuplicates(ListNode *head) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        if (!head)
+            return NULL;
         ListNode *dummy = new ListNode(0);
         dummy->next = head;
+    
         ListNode *curr = dummy;
-        while (curr) {
-            if (curr->next && curr->next->next) {
-                if (curr->next->val == curr->next->next->val) {
-                    ListNode *p = curr->next->next;
-                    while (p && curr->next->val == p->val) {
-                        p = p->next;    
-                    }
-                    curr->next = p;
-                } else {
-                    curr = curr->next;
+        while (curr->next && curr->next->next) {
+            if (curr->next->val == curr->next->next->val) {
+                ListNode *p = curr->next->next->next;
+                while (p && p->val == curr->next->val) {
+                    p = p->next;
                 }
+                curr->next = p;
             } else {
-                break;
+                curr = curr->next;
             }
         }
         return dummy->next;
